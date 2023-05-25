4a08338e1d8d221d0f70b457e54d3cd9f8225b40
khantadzeeren <miusern1@gmail.com>
2023-05-25 17:24:44 +0400

    category info config

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -2,7 +2,13 @@ import Category from "../models/Category.js";
 
 const GetAllCategories= async (req, res) => {
     const data = await Category.find();
-
+    const newData = data.map(category => {
+        return {
+            id: category.id,
+            name: category.name,
+            
+        }
+    })
     return res.status(200).json(data);
 }
 
